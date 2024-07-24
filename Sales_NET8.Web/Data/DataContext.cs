e147a98b58a3dad388d8d233124d95555329370f
vladimiro-fs <vladzyo.gaming@pm.me>
2024-07-24 17:27:25 +0100

    Countries controller

--- Sales_NET8.Web/Data/DataContext.cs
+++ Sales_NET8.Web/Data/DataContext.cs
@@ -11,5 +11,15 @@
         }
 
         public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasIndex(c => c.Name).IsUnique();
+        }
     }
+
+
+
+    
 }

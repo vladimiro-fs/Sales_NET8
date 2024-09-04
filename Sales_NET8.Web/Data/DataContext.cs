cf814c5de8115d8f7630481b6791bd4912bf58cf
vladimiro-fs <vladzyo.gaming@pm.me>
2024-09-04 21:35:14 +0100

    CRUD Categories

--- Sales_NET8.Web/Data/DataContext.cs
+++ Sales_NET8.Web/Data/DataContext.cs
@@ -12,10 +12,17 @@
 
         public DbSet<Country> Countries { get; set; }
 
+        public DbSet<Category> Categories { get; set; }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+
             modelBuilder.Entity<Country>()
+                .HasIndex(c => c.Name)
+                .IsUnique();
+
+            modelBuilder.Entity<Category>()
                 .HasIndex(c => c.Name)
                 .IsUnique();
         }
